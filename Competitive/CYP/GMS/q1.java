e8ed68b4c1884d9b7dc4dabb94d9270cfd839a20
shreyansh thakur <shreyanshthakur1@gmail.com>
2022-05-03 18:01:53 +0530

    push

--- Competitive/CYP/GMS/q1.java
+++ Competitive/CYP/GMS/q1.java
@@ -1,3 +1,58 @@
+import java.util.*;
+
 public class q1 {
-    
+
+    public boolean areAna(String s, String t) {
+
+        if (s.length() != t.length())
+            return false;
+
+        int[] freqs = new int[26];
+        int[] freqt = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freqs[s.charAt(i) - 'a']++;
+            freqt[t.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freqs[i] != freqt[i])
+                return false;
+        }
+
+        return true;
+
+    }
+
+    public List<List<String>> Anagrams(String[] string_list) {
+        // Code here
+
+        List<List<String>> res = new ArrayList<>();
+        // HashSet<String> set = new HashSet<>();
+        boolean[] vis = new boolean[string_list.length];
+
+        for (int i = 0; i < string_list.length; i++) {
+            if (vis[i])
+                continue;
+            String tocheck = string_list[i];
+            List<String> ans = new ArrayList<>();
+            ans.add(tocheck);
+            vis[i] = true;
+
+            for (int j = 0; j < string_list.length; j++) {
+                if (i == j)
+                    continue;
+
+                if (areAna(tocheck, string_list[j])) {
+                    vis[j] = true;
+                    ans.add(string_list[j]);
+                }
+            }
+
+            res.add(ans);
+        }
+
+        return res;
+
+    }
 }
