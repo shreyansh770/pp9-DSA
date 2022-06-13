96621de4c3e60f54c2775049ad75f5d42bd16831
shreyansh thakur <shreyanshthakur1@gmail.com>
2022-06-13 12:33:23 +0530

    push

--- Competitive/CYP/Amazon/q10.java
+++ Competitive/CYP/Amazon/q10.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class q10 {
-    
+    void matchPairs(char nuts[], char bolts[], int n) {
+        // code here
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            map.put(nuts[i],i);
+        }
+
+        for(int i=0;i<n;i++){
+
+            if(map.containsKey(bolts[i])){
+
+                nuts[i] = bolts[i];
+                
+            }
+        }
+
+        Arrays.sort(nuts);
+        Arrays.sort(bolts);
+    }
 }
---
+++ Competitive/Contests/JuneLong/q7.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q7 {
+        static class FastReader {
+            BufferedReader br;
+            StringTokenizer st;
+    
+            public FastReader() {
+                br = new BufferedReader(
+                        new InputStreamReader(System.in));
+            }
+    
+            String next() {
+                while (st == null || !st.hasMoreElements()) {
+                    try {
+                        st = new StringTokenizer(br.readLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return st.nextToken();
+            }
+    
+            int nextInt() {
+                return Integer.parseInt(next());
+            }
+    
+            long nextLong() {
+                return Long.parseLong(next());
+            }
+    
+            double nextDouble() {
+                return Double.parseDouble(next());
+            }
+    
+            String nextLine() {
+                String str = "";
+                try {
+                    str = br.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return str;
+            }
+        }
+        public static void main(String[] args) {
+            FastReader fs = new FastReader();
+
+            int t = fs.nextInt();
+
+            while(t-->0){
+
+                long x = fs.nextLong();
+                long n = fs.nextLong();
+
+                long tailCalls = 2 * (x-1);
+                long headCalls = (n-x/2);
+
+                long totalCalls = tailCalls * headCalls;
+
+                System.out.println(totalCalls);
+            }
+        }
+}
