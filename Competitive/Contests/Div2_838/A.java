ccd8a217563e32d916e409725b06d43e61f6f759
Shrey-zionn <shreyanshthakur1@gmail.com>
2022-12-15 20:29:28 +0530

    push

---
+++ Competitive/Contests/Div2_838/A.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class A{
+    public static void main(String[] args) {
+            Scanner scn = new Scanner(System.in);
+
+            int t  =scn.nextInt();
+            while(t-->0){
+                int n = scn.nextInt();
+
+                int[] ao = new int[n];
+                int[] ae = new int[n];
+                int s = 0;
+                int oc =0;
+                int ec = 0;
+                for(int i=0;i<n;i++){
+                    int a = scn.nextInt();
+                    if(a%2==0){
+                        ae[i] = 
+                    }else{
+                        oc++;
+                    }
+                }
+
+                if(s%2==0){
+                    System.out.println(0);
+                }else{
+
+                    if(oc%2==0){
+
+                    }
+
+                }
+            }
+    }
+}
