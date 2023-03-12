9588f481ba8a827321a7f7089db9e58cba428614
shreyansh770 <shreyanshthakur1@gmail.com>
2023-03-12 19:34:48 +0530

    code push

---
+++ Competitive/Ques/Gfg1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+//https://www.geeksforgeeks.org/place-k-elements-such-that-minimum-distance-is-maximized/
+public class Gfg1 {
+    
+    public static void main(String[] args) {
+            Scanner scn = new Scanner(System.in);
+
+            int n = scn.nextInt();
+            int[] a = new int[n];
+
+            for(int i=0;i<n;i++){
+                a[i] = scn.nextInt();
+            }
+
+            int k = scn.nextInt();
+            
+    }
+}
