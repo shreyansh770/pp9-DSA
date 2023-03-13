e45fc5c0115e4cc7b8f4403b72b5bd5675125980
shreyansh770 <shreyanshthakur1@gmail.com>
2023-03-13 23:45:27 +0530

    push

--- Competitive/Ques/Gfg1.java
+++ Competitive/Ques/Gfg1.java
@@ -13,6 +13,39 @@ public class Gfg1 {
             }
 
             int k = scn.nextInt();
+
+            Arrays.sort(a);
+
+            int low = 0;
+            int high = a[n-1]-a[0];
+            int res = 0;
+            while(low<=high){
+
+                int mid = (low+high)/2;
+                
+                int count =1;
+                int curr = a[0];
+
+                for(int i=1;i<n;i++){
+
+                    if(a[i]-curr>=mid){
+                        count++;
+                        curr = a[i];
+                    }
+
+                    if(count==k) break;
+                    
+                }
+
+                if(count>=k){
+                    res= mid;
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+
+            System.out.println(res);
             
     }
 }
