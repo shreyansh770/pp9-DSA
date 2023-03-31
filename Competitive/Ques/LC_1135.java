6fb38d3823638fa6d0601dcb41a4b72396bb45db
shreyansh770 <shreyanshthakur1@gmail.com>
2023-03-31 21:33:27 +0530

    inputs

--- Competitive/Ques/LC_1135.java
+++ Competitive/Ques/LC_1135.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class LC_1135 {
-    
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int e = scn.nextInt();
+        List<int[]> connections = new ArrayList<>();
+
+        for (int i = 0; i < e; i++) {
+            int u = scn.nextInt();
+            int v = scn.nextInt();
+            int c = scn.nextInt();
+            connections.add(new int[] { u, v, c });
+        }
+    }
 }
