512479793b35b80bbc1f73eac0a6733194fea770
shreyansh770 <shreyanshthakur1@gmail.com>
2023-04-17 01:37:52 +0530

    gfg2

---
+++ Competitive/Ques/Gfg2.java
@@ -0,0 +1,9 @@
+// https://www.geeksforgeeks.org/find-maximum-path-sum-in-a-binary-tree/
+import java.util.*;
+
+public class Gfg2 {
+    
+    public static void main(String[] args) {
+            Scanner scn = new Scanner(System.in);
+    }
+}
