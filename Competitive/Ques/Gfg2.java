e2f1994ee2a0c4fad37a4151e438cacfb106781d
shreyansh770 <shreyanshthakur1@gmail.com>
2023-04-17 17:45:18 +0530

    gfg 2 done

--- Competitive/Ques/Gfg2.java
+++ Competitive/Ques/Gfg2.java
@@ -1,9 +1,38 @@
+
 // https://www.geeksforgeeks.org/find-maximum-path-sum-in-a-binary-tree/
 import java.util.*;
 
 public class Gfg2 {
-    
+
+    class Node {
+
+        int data;
+        Node left, right;
+
+        public Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    static int max = 0;
+
+    public static int solve(Node root) {
+
+        if (root == null)
+            return 0;
+
+        int left = solve(root.left);
+        int right = solve(root.right);
+
+        int res = Math.max(root.data, Math.max(left + root.data, right + root.data));
+
+        max = Math.max(res, left + root.data + right);
+
+        return res;
+    }
+
     public static void main(String[] args) {
-            Scanner scn = new Scanner(System.in);
+        Scanner scn = new Scanner(System.in);
     }
 }
